3871b25b86e150ab4d859c7d507d8c08248c3ba5
morovati135 <mmorovati2233@gmail.com>
2025-08-11 10:15:11 +0330

    Add gitignore and product,user models

---
+++ WorkTest.Domain/Models/Products/Product.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.Users;
+
+namespace Domain.Models.Products;
+
+public class Product
+{
+    public int ProductId {get; set;}
+    public string Name {get; set;}
+    public decimal Price {get; set;}
+    
+    public int UserId {get; set;}
+    public User User {get; set;}
+}
---
+++ WorkTest.Domain/Models/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models.Products;
+
+namespace Domain.Models.Users;
+
+public class User
+{
+    public int UserId {get; set;}
+    public string UserName {get; set;}
+    public string Email {get; set;}
+    public string Password {get; set;}
+    
+    public ICollection<Product> Products {get; set;}
+    
+}
