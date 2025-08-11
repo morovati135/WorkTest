e9f93870907705f3713eec9fdf1da2127cf6a27c
morovati135 <mmorovati2233@gmail.com>
2025-08-11 10:24:07 +0330

    Add MappingProfile

---
+++ WorkTest.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dto;
+using AutoMapper;
+using Domain.Models.Products;
+using Domain.Models.Users;
+
+namespace Application.Mapping;
+
+public class MappingProfile: Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<CreateUserDto, User>();
+        CreateMap<UpdateUserDto, User>();
+
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<CreateProductDto, Product>();
+        CreateMap<UpdateProductDto, Product>();
+    } 
+}
